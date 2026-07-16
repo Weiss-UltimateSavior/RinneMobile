3f9b51cf90cb88fae233bf71fcf18f682801ad18
Weiss-UltimateSavior <weiss450006@gmail.com>
2026-07-16 12:33:19 +0800

    build(app): 修改版本号
    将当前版本号从0.9.9.2更新至0.9.9.3

--- app/src/main/java/com/yuki/yukihub/launcherbridge/LauncherUpdateBridge.java
+++ app/src/main/java/com/yuki/yukihub/launcherbridge/LauncherUpdateBridge.java
@@ -100,7 +100,7 @@ public final class LauncherUpdateBridge {
     }
 
     private static String getCurrentVersionName(Context context) {
-        return "0.9.9.2";
+        return "0.9.9.3";
     }
 
     private static boolean isNewerVersion(String latest, String current) {
